f441b2df97bcde8478529053b04fc4a3a642c631
dsavelyev <kriper0nind@gmail.com>
2023-08-02 16:19:54 +0300

    init

---
+++ app/users/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as process from 'process';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: parseInt(process.env.MICROSERVICES_USERS_PORT),
+      },
+    },
+  );
+
+  await app.listen();
+}
+bootstrap();
