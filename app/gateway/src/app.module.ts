e10b11728ae3560fdc165b37cc558a3256ec43fe
dsavelyev <kriper0nind@gmail.com>
2023-08-03 10:43:02 +0300

    added redis

--- app/gateway/src/app.module.ts
+++ app/gateway/src/app.module.ts
@@ -3,13 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
 import {ClientProxyFactory} from "@nestjs/microservices";
-import { UsersController } from './users/users.controller';
+import { UsersController } from './users.controller';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CacheModule.register<RedisClientOptions>({
+        // @ts-ignore
+        store: redisStore,
+        isGlobal: true,
+        host: process.env.CACHE_REDIS_HOST,
+        port: process.env.CACHE_REDIS_PORT,
+        password: process.env.CACHE_REDIS_PASSWORD
+      })
   ],
   controllers: [AppController, UsersController],
   providers: [AppService, {
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          [900]: "#10002b",
+          [800]: "#240046",
+          [700]: "#3c096c",
+          [600]: "#5a189a",
+          [500]: "#7b2cbf",
+          [400]: "#9d4edd",
+          [300]: "#c77dff",
+          [200]: "#e0aaff",
+          [100]: "#eed6ff"
+        }
+      }
+    },
   },
   plugins: [],
 }
